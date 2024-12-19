9f0aea1f66a0f54bb0e0e485a6062ee4587870f6
sukherdenei <sukherdene.ba@gmail.com>
2024-12-20 07:03:19 +0800

    new

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./app.css";
+import { Header } from "./components/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Cover />
+      <Story />
+    </div>
+  );
+}
+export default App;
